67293a13c9d42bb49e72148080a00054f2bd61f4
Yuan Lin <yuanlin@kinMbp.local>
2017-08-28 21:17:03 +1200

    initial

---
+++ Script/jquery.scroll.js
@@ -0,0 +1,90 @@
+﻿//垂直方向滚动 
+$.fn.extend({
+
+    scrollVertical: function(speed) 
+    { 
+        if (arguments.length < 1)//如果没有指定速度，按默认的速度进行滚动 
+        { 
+           var speed = 10; 
+        }
+      
+        function vertial(This) 
+        { 
+            //alert("fask"); 
+            //alert($(This.children().get(1)).outerHeight()); 
+            if ($(This.children().get(1)).outerHeight() - This.scrollTop() <= 0)//如果复制得到的元素显示的高度比整个div隐掉的高度小或相等，则heng1已完全隐藏，这时需要复位动画前的位置(即heng.scrollTop置零)，重新开始动画 
+            { 
+                This.scrollTop(0); 
+            } 
+            else//如果复制得到的元素显示的高度比整个div隐掉的高度大，heng1还没有完全隐藏，heng.scrollTop继续增加 
+            { 
+                This.scrollTop(This.scrollTop() + 1); 
+            } 
+        }
+
+        $(this).wrap("<div id='scrollvarea' style='OVERFLOW: hidden; COLOR: #ffffff;'></div>"); 
+        $("#scrollvarea").height($("#scrollvarea").children().get(0).scrollHeight);
+
+        //alert($("#scrollvarea").children().get(0).scrollHeight);
+
+        $(this).clone().insertAfter(this); 
+        var scrollVerticalhandle = setInterval(function() { vertial($("#scrollvarea")); }, speed); 
+        $("#scrollvarea").mouseover(function() { clearInterval(scrollVerticalhandle); }); 
+        $("#scrollvarea").mouseout(function() { scrollVerticalhandle = setInterval(function() { vertial($("#scrollvarea")); }, speed); }); 
+    }
+
+});
+
+//水平方向滚动 
+$.fn.extend({
+
+    scrollHorizontal: function(speed) { 
+        if (arguments.length < 1)//如果没有指定速度，按默认的速度进行滚动 
+        { 
+            var speed = 50; 
+        }
+
+        function horizontal(This) { 
+            //alert("fask"); 
+            //alert($(This.children().get(1)).outerHeight()); 
+            if ($($("#content").children().get(0)).outerWidth() - This.scrollLeft() <= 0)//如果复制得到的元素显示的宽度比整个#scrollharea隐掉的宽度小或相等，则原来的块已完全隐藏，这时需要复位动画前的位置(即#scrollharea的scrollTop置零)，重新开始动画 
+            { 
+               //alert("afsd"); 
+                This.scrollLeft(0); 
+                //alert("afsd"); 
+            } 
+            else//如果复制得到的元素显示的宽度比整个div隐掉的宽度大，原来的块还没有完全隐藏，#scrollharea的scroll继续增加 
+            { 
+                This.scrollLeft(This.scrollLeft() + 1); 
+            } 
+        } 
+        //先用#scrollharea包裹所要滚动的对象 
+        $(this).wrap("<div id='scrollharea' style='OVERFLOW: hidden; COLOR: #ffffff;'></div>"); 
+        $("#scrollharea").height($("#scrollharea").children().get(0).scrollHeight); 
+        $("#scrollharea").width($("#scrollharea").children().get(0).scrollWidth); 
+        //复制滚动的对象 
+        $(this).clone().insertAfter(this); 
+        //将要滚动的对象和复制得到的对象用一个#content块包裹(这样才能设其宽度，使jquery的scrollTop()有效) 
+        $("#scrollharea").children().wrapAll("<div id='content' style='OVERFLOW: hidden;text-align:left;'></div>"); 
+        $("#content").height($("#content").children().get(0).scrollHeight); 
+        $("#content").width($("#content").children().get(0).scrollWidth*2);//宽度为每个块的二倍 
+        //将对象排列在一行 
+        $("#content").children().css("float", "left"); 
+        //控制动画过程 
+        var scrollHorizontalhandle = setInterval(function() { horizontal($("#scrollharea")); }, speed);
+
+        $("#scrollharea").mouseover(function() { 
+            clearInterval(scrollHorizontalhandle); 
+        } 
+        );
+
+        $("#scrollharea").mouseout(function() { 
+            scrollHorizontalhandle = setInterval(function() { horizontal($("#scrollharea")); }, speed); 
+        } 
+        ); 
+    }
+
+});
+
+// JScript 文件
+
---
+++ js/ad.js
@@ -0,0 +1,133 @@
+﻿/// <reference path="jquery-1.4.2.min.js" />
+var xPos = parseInt(window.screen.availWidth * Math.random());
+var yPos = parseInt(window.screen.availHeight * Math.random());
+var xon = 0;
+var yon = 0;
+function changePos(obj) {
+    var clientW = window.screen.availWidth - 20;
+    var clientH = window.screen.availHeight - 100;
+    var offsetW = document.getElementById(obj).offsetWidth;
+    var offsetH = document.getElementById(obj).offsetHeight;
+    document.getElementById(obj).style.left = xPos + document.body.scrollLeft;
+    document.getElementById(obj).style.top = yPos + document.body.scrollTop;
+
+    if (xon) { xPos = xPos + 1; } else { xPos = xPos - 1; }
+    if (xPos < 0) { xon = 1; xPos = 0; }
+    if (xPos >= (clientW - offsetW)) { xon = 0; xPos = (clientW - offsetW); }
+
+    if (yon) { yPos = yPos + 1; } else { yPos = yPos - 1; }
+    if (yPos < 0) { yon = 1; yPos = 0; }
+    if (yPos >= (clientH - offsetH)) { yon = 0; yPos = (clientH - offsetH); }
+}
+function floatAd(obj) {
+    window.setInterval("changePos('" + obj + "')", "15");
+}
+
+
+var lastScrollY = 0;
+function distichAd(obj1, obj2) {
+    var diffY;
+    if (document.documentElement && document.documentElement.scrollTop)
+        diffY = document.documentElement.scrollTop;
+    else if (document.body)
+        diffY = document.body.scrollTop
+    else
+    { /*Netscape stuff*/ }
+    percent = .1 * (diffY - lastScrollY);
+    if (percent > 0) percent = Math.ceil(percent);
+    else percent = Math.floor(percent);
+    document.getElementById(obj1).style.top = parseInt(document.getElementById(obj1).style.top) + percent + "px";
+    document.getElementById(obj2).style.top = parseInt(document.getElementById(obj2).style.top) + percent + "px";
+    lastScrollY = lastScrollY + percent;
+}
+function initEcAd(obj1, obj2) {
+    window.setInterval("distichAd('" + obj1 + "','" + obj2 + "')", 1);
+}
+var adTimer;
+var cur_img = -1;
+
+function PicHover(obj1, obj2, obj3, obj4) {
+    var n = 0;
+    var m = 0;
+    var k = 0;
+    var aCount = $(obj1 + " ul li").length;
+    $(obj1 + " ul li").mouseover(function () {
+        var i = $(obj1 + " ul li").index(this);
+        PicRoll(obj1, obj2, i);
+    });
+
+    $(obj1).hover(function () {
+        clearInterval(adTimer);
+    }, function () {
+        adTimer = setInterval(function () {
+            n++;
+            if (n == aCount) { n = 0 };
+            PicRoll(obj1, obj2, n);
+        }, 8000);
+    }).trigger("mouseleave");
+
+
+    $(obj3).click(function () {
+        cur_img++;
+        if (cur_img == aCount) { cur_img = 0; }
+        PicRoll(obj1, obj2, cur_img);
+    });
+
+    $(obj3).mouseover(function () {
+        $('.next').css({ 'opacity': 0.5 });
+    });
+
+    $(obj3).mouseout(function () {
+        $('.next').css({ 'opacity': 0.2 });
+        clearInterval(adTimer);
+    }, function () {
+        adTimer = setInterval(function () {
+            m++;
+            if (m == aCount) { m = 0 };
+            PicRoll(obj1, obj2, m);
+        }, 8000);
+    }).trigger("mouseleave");
+
+    $(obj4).click(function () {
+        cur_img--;
+        if (cur_img < 0) { cur_img = $(obj1 + " ul li").length - 1; }
+        PicRoll(obj1, obj2, cur_img);
+    });
+
+    $(obj4).mouseover(function () {
+        $('.prev').css({ 'opacity': 0.5 });
+    });
+
+    $(obj4).hover(function () {
+        $('.prev').css({ 'opacity': 0.2 });
+        clearInterval(adTimer);
+    }, function () {
+        adTimer = setInterval(function () {
+            k++;
+            if (k == aCount) { k = 0 };
+            PicRoll(obj1, obj2, k);
+        }, 8000);
+    }).trigger("mouseleave");
+}
+
+
+function PicRoll(obj1, obj2, i) {
+    cur_img = i;
+    $(obj1 + " ul li").attr("class", "normal");
+    $(obj1 + " ul li").eq(i).attr("class", "selected");
+
+    $(obj2 + " ul li").hide();
+    $(obj2 + " ul li").eq(i).fadeIn("normal");
+}
+function Stoptrigger() {
+    clearInterval(adTimer);
+}
+function startrigger(obj1, obj2) {
+    var n = 0;
+    var aCount = $(obj1 + " ul li").length;
+    adTimer = setInterval(function () {
+        n++;
+        if (n == aCount) { n = 0 };
+        PicRoll(obj1, obj2, n);
+    }, 8000);
+}
